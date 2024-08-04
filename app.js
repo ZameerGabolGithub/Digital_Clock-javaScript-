7c7890bae63310408d292ab902814a1f7980077d
ZameerGabolGithub <143511847+ZameerGabolGithub@users.noreply.github.com>
2024-08-04 16:57:19 +0500

    Digital_Clock Program...
    Digital_Clock Program html ,Css, JavaScript.

---
+++ app.js
@@ -0,0 +1,18 @@
+//Digital Clock program
+
+function updateClock(){
+const now = new Date();
+let hours = now.getHours()
+const maridies = hours >=12 ? "PM" : "AM";
+hours = hours % 12 ||12;
+hours=hours.toString().padStart(2,0);
+const minutes = now.getMinutes().toString().padStart(2,0);
+const seconds = now.getSeconds().toString().padStart(2,0);
+const timeString = `${hours}:${minutes}:${seconds} ${maridies}`
+document.getElementById("")
+document.getElementById("clock").textContent=timeString;
+
+}
+
+updateClock();
+setInterval(updateClock,1000);
